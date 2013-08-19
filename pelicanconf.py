4d6097c4ce34602c9f1886f1d0e11db137a98390
chencheng <chencheng@chenchengmatoMacBook-Pro.local>
2013-08-19 20:24:43 +0800

    your comments

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,15 +17,36 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
+PLUGIN_PATH = u"pelican-plugins"
+PLUGINS = ["sitemap"]
+
+## 配置sitemap 插件
+SITEMAP = {
+    "format": "xml",
+    "priorities": {
+        "articles": 0.7,
+        "indexes": 0.5,
+        "pages": 0.3,
+    },
+    "changefreqs": {
+        "articles": "monthly",
+        "indexes": "daily",
+        "pages": "monthly",
+    }
+}
+
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  (('Google', 'https://www.google.com/ncr'),
+          ('Python', 'http://python.org/'),
+          ('Pelican', 'http://docs.getpelican.com/'),
+          ('lizherui的程序世界', 'http://www.lizherui.com/')
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Github', 'https://github.com/lizherui'),
+          ('Twitter', 'https://twitter.com/iamccme'),
+          ('Weibo', 'http://weibo.com/iamccme'),
+         )
 
 DEFAULT_PAGINATION = 10
 
